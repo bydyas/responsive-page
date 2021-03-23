c354de719876616da26cb62d9e98130c9f2ec60c
bydyas <bogdan.shkaran@gmail.com>
2021-03-23 21:34:49 +0200

    added slider

---
+++ markup/js/slick-config.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        responsive: [
+            {
+            breakpoint: 1230,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+            },
+            {
+            breakpoint: 830,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+            },
+        ]
+    });
+});
